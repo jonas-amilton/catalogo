74579961b57060f347f584f61a7fc5491e080cfa
jonas-amilton <jonas96@edu.unisinos.br>
2025-09-27 18:07:22 -0300

    feat: criado controller para separar endpoints utilizados pelo admin

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    use AuthorizesRequests;
+
+    //
+
+}
